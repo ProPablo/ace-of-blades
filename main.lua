eff4ab3309c96c0b60e5f8bb1047a6825042b311
sh1ggy <tyronewessnolasco@gmail.com>
2025-08-08 23:50:45 +1000

    updoot

--- main.lua
+++ main.lua
@@ -4,7 +4,15 @@
 
 local game = {}
 
+local val = 0 -- establish a variable for later use
+local x, y
+valSpeed = 500
+
+blocksWidth = 50
+
 function love.load()
+    if arg[#arg] == "-debug" then require("mobdebug").start() end
+    require("util")
     Camera = require("libs/hump/camera")
     Gamestate = require("libs/hump/gamestate")
     vector = require "libs.hump.vector"
@@ -12,7 +20,6 @@ function love.load()
         width = love.graphics.getWidth(),
         height = love.graphics.getHeight()
     }
-
 
     world = love.physics.newWorld(0, 1, true) -- create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
 
@@ -28,7 +35,7 @@ function setupBlocks()
     blocks.block1 = {}
     blocks.block1.body = love.physics.newBody(world, screen.width / 2, 0, "static")
     blocks.block1.shape = love.physics.newRectangleShape(0, 0, screen.width, blocksWidth)
-    blocks.block1.fixture = love.physics.newFixture(blocks.block1.body, blocks.block1.shape, 5) -- A higher density gives it more mass.
+    blocks.block1.fixture = love.physics.newFixture(blocks.block1.body, blocks.block1.shape, 5)
 
     blocks.block2 = {}
     blocks.block2.body = love.physics.newBody(world, 0, screen.height / 2, "static")
@@ -38,10 +45,28 @@ function setupBlocks()
     blocks.block3 = {}
     blocks.block3.body = love.physics.newBody(world, screen.width / 2, screen.height, "static")
     blocks.block3.shape = love.physics.newRectangleShape(0, 0, screen.width, blocksWidth)
-    blocks.block3.fixture = love.physics.newFixture(blocks.block3.body, blocks.block3.shape, 5) -- A higher density gives it more mass.
+    blocks.block3.fixture = love.physics.newFixture(blocks.block3.body, blocks.block3.shape, 5) 
 
     blocks.block4 = {}
     blocks.block4.body = love.physics.newBody(world, screen.width, screen.height / 2, "static")
     blocks.block4.shape = love.physics.newRectangleShape(0, 0, blocksWidth, screen.height)
     blocks.block4.fixture = love.physics.newFixture(blocks.block4.body, blocks.block4.shape, 5)
+end
+
+function love.draw()
+    love.graphics.print(val, 100, 200)
+    love.graphics.print(x, 100, 300)
+    love.graphics.print(y, 100, 400)
+    for _, block in ipairs(blocks) do
+        print(block)
+        love.graphics.rectangle("fill", block.x, block.y, block.width, block.height)
+    end
+end
+
+function love.update(dt)
+    world:update(dt) -- this puts the world into motion
+    x, y = love.mouse.getPosition()
+    if love.mouse.isDown(1) then
+        val = val + dt * valSpeed -- we will increase the variable by 1 for every second the button is held down
+    end
 end
