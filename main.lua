e01242dc10a6baf5a2acd95174815f080dd82878
sh1ggy <tyronewessnolasco@gmail.com>
2025-08-09 02:12:48 +1000

    puush

--- main.lua
+++ main.lua
@@ -1,19 +1,31 @@
 -- Run this to hot reload
 -- nodemon --exec "love ." --ext lua --ignore node_modules
-
 game = {}
 menu = {}
 
 isServer = false
 
-local val = 0 -- establish a variable for later use
-local x, y
+-- GAME STATE
+hasRipped = false
+
+-- FORCE VALUES
+val = 0 -- establish a variable for later use
+cursorX, cursorY = 0
+startDragX = 0
+startDragY = 0
+endDragX = 0
+endDragY = 0
 
 valSpeed = 500
-forceMod = 1000
+forceMod = 10000
+
+isDragging = false
+hasRipped = false
 
 function love.load()
-    if arg[#arg] == "-debug" then require("mobdebug").start() end
+    if arg[#arg] == "-debug" then
+        require("mobdebug").start()
+    end
     require("util")
     require("blade")
     require("blocks")
@@ -37,22 +49,46 @@ function game:enter()
 end
 
 function game:draw()
-    love.graphics.print(val, 100, 200)
-    love.graphics.print(x, 100, 300)
-    love.graphics.print(y, 100, 400)
+    drawDebug()
     drawBlocks()
     drawBlade()
+    drawRip()
 end
 
 function game:update(dt)
     world:update(dt)
-    x, y = love.mouse.getPosition()
-    if love.mouse.isDown(1) then
-        val = val + dt * valSpeed -- we will increase the variable by 1 for every second the button is held down
+    cursorX, cursorY = love.mouse.getPosition()
+    if (hasRipped) then
+        return
     end
 
-    if not love.mouse.isDown(1) then
-        beyblade.body:applyForce(val*forceMod, val*forceMod)
-        val = 0
+    if (love.mouse.isDown(1) and isDragging == false) then
+        beyblade.body:setPosition(cursorX, cursorY)
+        startDragX = cursorX
+        startDragY = cursorY
+        isDragging = true
     end
+
+    -- RELEASE
+    if (not love.mouse.isDown(1) and isDragging == true) then
+        endDragX = cursorX
+        endDragY = cursorY
+        local dx = endDragX - startDragX
+        local dy = endDragY - startDragY
+        -- Distance between click down and release
+        local length = dx + dy
+        if length > 0 then -- Ignoring same spot start & end
+            print(length)
+            -- Normalize and apply force in the opposite direction
+            -- dx / length is a direction vector 
+            local fx = -dx * forceMod
+            local fy = -dy * forceMod
+            beyblade.body:applyForce(fx, fy)
+        else
+            print(length)
+        end
+        hasRipped = true
+        isDragging = false
+    end
+
 end
--- util.lua
+++ util.lua
@@ -5,6 +5,16 @@ function setToSecondMonitor()
     love.window.setMode(400, 400, {
         x = desktopWidth + 30,
         y = 30,
-        resizable = true,
+        resizable = true
     })
+end
+
+function drawDebug()
+    love.graphics.print(val, 100, 200)
+    love.graphics.print(cursorX, 100, 300)
+    love.graphics.print(cursorY, 150, 300)
+    love.graphics.print(startDragX, 100, 400)
+    love.graphics.print(startDragY, 150, 400)
+    love.graphics.print(endDragX, 100, 500)
+    love.graphics.print(endDragY, 150, 500)
 end
